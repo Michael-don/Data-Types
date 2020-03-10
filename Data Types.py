5f07c579c09db346f2b35d01e42ebdfa01aeff9b
michael <michaeldon0204@gmail.com>
2020-03-10 01:20:36 -0700

    'commit'

---
+++ Data Types.py
@@ -0,0 +1,31 @@
+# declaring a string variable
+x="Lifechoices Coding Academy"
+print(type(x))
+print(x[0:4])
+
+# declaring a integer variable
+number1=5
+
+# declaring a float variable
+number2=5.8
+
+# declaring a complex variable
+number3=3e+7j
+print(type(number1))
+print(type(number2))
+print(type(number3))
+
+
+mylist=[23, 82, 42, 84, 33]
+mylist.insert(0, 23)
+print(mylist)
+
+# declaring a set variable
+cars={"Aston Martin", "VW", "Ford", "BMW", "Nissan"}
+print(type(cars))
+
+# declaring a tuple variable
+cars1=("Aston Martin", "VW", "Ford", "BMW", "Nissan")
+print(type(cars1))
+
+
